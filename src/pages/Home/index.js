40079ce6f996eed4c21d61c579ecba055bfbb903
Budi-Laptop <budi.salah@outlook.com>
2022-09-02 20:43:35 +0200

    import the main component and add simple style in Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,15 @@
+import Poll from '../../components/Poll';
+import Vote from '../../components/Vote';
+import Chart from '../../components/Chart';
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main className="grid grid-cols-3">
+      <Poll />
+      <Vote />
+      <Chart />
+    </main>
+  );
 }
 
 export default Home;
