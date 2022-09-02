af434a28b3e7a503e2ef1c9e1950e22e3999896a
Budi-Laptop <budi.salah@outlook.com>
2022-09-02 20:48:01 +0200

    create question input section in Poll component

--- src/components/Poll/index.js
+++ src/components/Poll/index.js
@@ -1,5 +1,22 @@
+import { useContext } from 'react';
+import { MainContext } from '../MainContext';
+import Section from '../UI/Section';
+import TextInput from '../units/TextInput';
+
 function Poll() {
-  return <h1>Hello, poll</h1>;
+  const { question, setQuestion } = useContext(MainContext);
+
+  return (
+    <Section className="flex flex-col flex-wrap items-stretch">
+      <TextInput
+        label="question"
+        state={question}
+        stateHandler={setQuestion}
+        name="question-input"
+        placeholder="Please enter your question..."
+      />
+    </Section>
+  );
 }
 
 export default Poll;
