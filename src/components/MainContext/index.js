a52bfa4bf819b26dc832b9b03be9cc185bac15c3
Budi-Laptop <budi.salah@outlook.com>
2022-09-03 14:09:47 +0200

    extend MainContext

--- src/components/MainContext/index.js
+++ src/components/MainContext/index.js
@@ -4,10 +4,13 @@ export const MainContext = createContext();
 
 function MainProvider({ children }) {
   const [question, setQuestion] = useState('');
+  const [answers, setAnswers] = useState([]);
 
   const defaultObj = {
     question,
     setQuestion,
+    answers,
+    setAnswers,
   };
 
   return (
